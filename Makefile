48aa73894fe5b6e52888f8a8428ef479ef5622e8
Jonathan Nicolas <jnicolas@c3r12p2.42quebec.com>
2023-04-23 21:38:57 -0400

    Starting cub3d

---
+++ Makefile
@@ -0,0 +1,53 @@
+NAME = cub3d
+LIBFTNAME = libft.a
+LIBFTPATH = ./libft
+
+SRC_DIR = ./src/
+SRC = test.c
+
+OBJS = ${addprefix ${SRC_DIR}, ${SRC:.c=.o}}
+
+# /* ~~~~~~~ INCLUDING GNL ~~~~~~~ */
+GNL_DIR = ./gnl/
+GNL = get_next_line.c \
+	get_next_line_utils.c \
+
+GNL_OBJS = ${addprefix ${GNL_DIR}, ${GNL:.c=.o}}
+
+
+FLAGS = -Wall -Wextra -Werror -g
+LINKS = -framework OpenGL -framework AppKit
+#MLB_FLAGS	= -I -g -L /usr/X11/lib -L./mlbx -lmlx -Imlx -lXext -lX11 -lz -lm
+
+
+NONE='\033[0m'
+GREEN='\033[32m'
+GRAY='\033[2;37m'
+CURSIVE='\033[3m'
+
+all: $(NAME)
+
+$(NAME): $(OBJS) $(GNL_OBJS)
+	@echo $(CURSIVE)$(GRAY) "     - Compiling $(NAME)..." $(NONE)
+	@make -C ${LIBFTPATH}
+	@gcc $(FLAGS) $(OBJS) $(GNL_OBJS) ./libft/libft.a ./minilibx_opengl/libmlx.a $(LINKS) -o $(NAME)
+	@echo $(GREEN)"- Compiled -"$(NONE)
+	@rm $(OBJS)
+	@echo $(CURSIVE)$(GRAY) "     Deleted object files" $(NONE)
+
+exe: all
+	@echo "     - Executing $(NAME)... \n"
+	@./$(NAME)
+	@echo "\n     - Done -"
+
+clean:
+	@echo $(CURSIVE)$(GRAY) "     - Removing object files..." $(NONE)
+	@rm -rf $(OBJS)
+
+fclean: clean
+	@echo $(CURSIVE)$(GRAY) "     - Removing $(NAME)..." $(NONE)
+	@rm -rf $(NAME)
+	@cd libft && make fclean
+	@cd minilibx_opengl && make clean
+
+re: fclean all
---
+++ cub3d.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnicolas <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 18:48:42 by jnicolas          #+#    #+#             */
+/*   Updated: 2022/10/25 17:10:44 by jnicolas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "minilibx_opengl/mlx.h"
+# include <stdlib.h>
+# include <stdio.h>
+
+# define X_EVENT_KEY_PRESS	2
+# define X_EVENT_KEY_EXIT	17
+# define KEY_ESC			53
+# define KEY_W				13
+# define KEY_A				0
+# define KEY_S				1
+# define KEY_D				2
+
+typedef struct s_data {
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_data;
+
+typedef struct s_color
+{
+	unsigned char	blue;
+	unsigned char	green;
+	unsigned char	red;
+	unsigned char	blank;
+}	t_color;
+
+typedef union u_rgb
+{
+	int		rgb;
+	t_color	rgbs;
+}	t_rgb;
+#endif
