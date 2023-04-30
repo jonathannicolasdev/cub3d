5d2cb046cab26f2ed7bb71f2d4d91606b1080924
Jonathan Nicolas <jnicolas@c3r12p2.42quebec.com>
2023-04-29 23:51:53 -0400

    Add init player camera and ray

--- cub3d.h
+++ cub3d.h
@@ -13,51 +13,69 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "minilibx_opengl/mlx.h"
-# include <stdlib.h>
-# include <stdio.h>
 # include "libft/libft.h"
-# include <string.h>
+# include "minilibx_opengl/mlx.h"
 # include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 
-# define X_EVENT_KEY_PRESS	2
-# define X_EVENT_KEY_EXIT	17
-# define KEY_ESC			53
-# define KEY_W				13
-# define KEY_A				0
-# define KEY_S				1
-# define KEY_D				2
+# define X_EVENT_KEY_PRESS 2
+# define X_EVENT_KEY_EXIT 17
+# define KEY_ESC 53
+# define KEY_W 13
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
 
+# define screen_width 640
+# define screenHeight 480
+# define RED 0Xff0000
+# define GREEN 0X454B1B
+# define BLUE 0X0000FF
 
-#define screenWidth 640
-#define screenHeight 480
-#define RED 0Xff0000
-#define GREEN 0X454B1B
-#define BLUE 0X0000FF
+typedef struct s_data
+{
+	void			*mlx;
+	void			*win;
+	void			*img;
+	char			*addr;
+	int				bpp;
+	int				line_len;
+	int				endian;
+	t_player		*player;
+}					t_data;
 
-typedef struct s_data {
-	void	*mlx;
-	void	*win;
-	void	*img;
-	char	*addr;
-	int		bpp;
-	int		line_len;
-	int		endian;
+typedef struct s_camera
+{
+	double			camera_x;
+	double			camera_y;
+	double			camera_dir_x;
+	double			camera_dir_y;
+	double			camera_plane_x;
+	double			camera_plane_y;
+}					t_camera;
+
+typedef struct s_player
+{
+	double	vector_dir_x;
+	double	vector_dir_y;
 	double	pos_x;
 	double	pos_y;
-	double	player_dir_x;
-	double	player_dir_y;
-}	t_data;
+	int		map_x;
+	int		map_y;
+}	t_player;
 
-typedef struct s_camera {
-	double	camera_x;
-	double	camera_y;
-	double	camera_dir_x;
-	double	camera_dir_y;
-	double	camera_plane_x;
-	double	camera_plane_y;
-} t_camera
-
+typedef struct s_ray
+{
+	double			camera_x;
+	double			ray_dir_x;
+	double			ray_dir_y;
+	double			side_dist_s;
+	double			side_dist_y;
+	double			delta_dist_x;
+	double			delta_dist_y;
+}					t_ray;
 
 typedef struct s_color
 {
@@ -65,21 +83,22 @@ typedef struct s_color
 	unsigned char	green;
 	unsigned char	red;
 	unsigned char	blank;
-}	t_color;
+}					t_color;
 
 typedef union u_rgb
 {
-	int		rgb;
-	t_color	rgbs;
-}	t_rgb;
+	int				rgb;
+	t_color			rgbs;
+}					t_rgb;
 
-typedef struct s_map {
-	char	**map;
-	int		width;
-	int		height;
-}	t_map;
+typedef struct s_map
+{
+	char			**map;
+	int				width;
+	int				height;
+}					t_map;
 
-t_map		*read_map(char *filename);
-t_camera	*init_camera(t_data *data)
+t_map				*read_map(char *filename);
+t_camera			*init_camera(t_player *player);
 
 #endif
--- src/raycasting/init_raycasting.c
+++ src/raycasting/init_raycasting.c
@@ -12,17 +12,40 @@
 
 #include "../../cub3d.h"
 
-t_camera	*init_camera(t_data *data)
+t_player	*init_player(double pos_x, double pos_y, double dir_x, double dir_y)
+{
+	t_player	*player;
+
+	player = malloc(sizeof(*player));
+	player->pos_x = pos_x;
+	player->pos_y = pos_y;
+	player->vector_dir_x = dir_x;
+	player->vector_dir_y = dir_y;
+	player->map_x = (int)pos_x;
+	player->map_y = (int)pos_y;
+	return (player);
+}
+
+t_camera	*init_camera(t_player *player)
 {
 	t_camera	*camera;
 
 	camera = malloc(sizeof(*camera));
 	if (!camera)
 		return (NULL);
-	camera->camera_plane_x = data->player_dir_y * 0.66;
-	camera->camera_plane_y = -(data->player_dir_x * 0.66);
-	camera->camera_dir_x = data->player_dir_x;
-	camera->camera_dir_y = data->player_dir_y;
-	camera->camera_x = data->pos_x;
-	camera->camera_y = data->pos_y;
+	camera->camera_plane_x = player->vector_dir_x * 0.66;
+	camera->camera_plane_y = -(player->vector_dir_y * 0.66);
+	return (camera);
+}
+
+t_ray	*init_ray(t_player *player, t_camera *camera, int x_screen)
+{
+	double	camera_x;
+	t_ray	*ray;
+
+	ray = malloc(sizeof(*ray));
+	camera_x = 2 * x_screen / (double)screen_width - 1;
+	ray->ray_dir_x = player->vector_dir_x + camera->camera_plane_x * camera_x;
+	ray->ray_dir_y = player->vector_dir_y + camera->camera_plane_y * camera_x;
+	return (ray);
 }
