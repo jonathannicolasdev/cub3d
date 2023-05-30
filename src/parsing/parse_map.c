33d522bbd350d0f8dfb21ee0e838d7c5af5359c2
Yanis Labbe <ylabbe@c1r5p7.42quebec.com>
2023-05-29 20:07:51 -0400

    fix map segfault

--- src/parsing/parse_map.c
+++ src/parsing/parse_map.c
@@ -12,23 +12,52 @@
 
 #include "../../cub3d.h"
 
+int	ft_map_zero_last_first(char **map, int y)
+{
+	int	i;
+
+	i = 0;
+	while (map[y - 1][i] != '\0')
+	{
+		if (map[y - 1][i] == '0')
+		{
+			return (1);
+		}
+		i++;
+	}
+	i = 0;
+	while (map[0][i])
+	{
+		if (map[0][i] == '0')
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 void	ft_map_check(t_parse *parse, char **map, int actual, int y)
 {
 	int	i;
 
 	i = 0;
+	if (ft_map_zero_last_first(map, y) == 1)
+	{
+		parse->map_wg_space = 1;
+		return ;
+	}
 	while (map[actual][i] != '\0')
 	{
-		if (ft_map_corner(map, actual, i) == 1)
+		if ((ft_map_corner(map, actual, i) == 1)
+			|| (ft_map_char(map, actual, i) == 1)
+			|| (ft_map_space(map, actual, i, y) == 1)
+			|| (ft_map_player(map, actual, i, y) == 1)
+			|| (ft_map_zero(map, actual, i, y) == 1))
+		{
 			parse->map_wg_corner = 1;
-		if (ft_map_char(map, actual, i) == 1)
-			parse->map_wg_character = 1;
-		if (ft_map_space(map, actual, i, y) == 1)
-			parse->map_wg_space = 1;
-		if (ft_map_player(map, actual, i, y) == 1)
-			parse->map_wg_player = 1;
-		if (ft_map_zero(map, actual, i, y) == 1)
-			parse->map_wg_zero = 1;
+			return ;
+		}
 		i++;
 	}
 }
@@ -43,7 +72,9 @@ t_parse	*ft_map(t_parse *parse, char **map)
 	if (ft_map_total_position(map) == 1)
 		parse->map_wg_player_number = 1;
 	while (map[y] != NULL)
+	{
 		y++;
+	}
 	while (map[actual])
 	{
 		ft_map_check(parse, map, actual, y);
